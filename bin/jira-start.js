5766d6d7020778bffaa364bd373f6b9fd319eb42
danpickford <social@danpickford.com>
2017-03-04 16:48:41 +0000

    Basic save on timer create

--- bin/jira-start.js
+++ bin/jira-start.js
@@ -3,6 +3,9 @@
 const program = require('commander');
 const PACKAGE = require('../package');
 const chalk = require('chalk');
+const Regex = require("regex");
+const Jiratime = require('../lib/jiratime');
+const jiratime = global.jiratime = new Jiratime();
 
 let jira_number = 0;
 program
@@ -14,3 +17,11 @@ program
     })
   	.parse(process.argv);
 
+//let regex = new Regex(/(?:\s|^)([A-Z]+-[0-9]+)(?=\s|$)/g);
+let regex = /(?:\s|^)([A-Z]+-[0-9]+)(?=\s|$)/g;
+//if (!regex.test(jira_number)){
+if (!jira_number.match(regex)){
+	jiratime.criticalError('The Jira number is not valid ' + chalk.underline.bold(jira_number) + '.');
+}
+
+jiratime.startTimer(jira_number);
